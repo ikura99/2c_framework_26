4e5a2543998a3664e128c848888c61cd25b7a998
ikura99 <takahashirinto220@gmail.com>
2023-11-17 14:34:46 +0900

    edit#5

---
+++ MyGitProject2c/src/main/java/com/example/demo/controller/gitItemEditController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class gitItemEditController {
+
+	@RequestMapping(path = "/gititemedit", method = RequestMethod.GET)
+	public String Edit() {
+		return "gititemedit";
+	}
+
+	@RequestMapping(path = "/gititemedit2", method = RequestMethod.POST)
+	public String Edit2(String name, int price, Model model) {
+		
+		model.addAttribute("name", name);	
+		model.addAttribute("price", price);
+		
+		return "gititemedit2";
+	}
+	
+	
+	
+}
